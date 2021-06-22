6fa9dd964350f6babc232c214c2c77146a25a7be
Amar Nath Yogi <contact@cyogian.dev>
2021-06-22 20:41:09 +0530

    all files processed

--- percentile.py
+++ percentile.py
@@ -3,10 +3,10 @@ import numpy as np
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
-df = pd.read_excel('dec.xlsx', sheet_name="Sheet1")
+df = pd.read_excel('deci6.xlsx', sheet_name="Sheet1")
 
 df = pd.DataFrame(df)
 
-df['Percentile Rank'] = df.dec.rank(method='first', pct=True)
+df['Percentile Rank'] = df.deci.rank(method='first', pct=True)
 df['Decile Rank'] = pd.qcut(df['Percentile Rank'], 10, labels=False)
-df.to_excel("dec_output.xlsx")
+df.to_excel("deci6_output.xlsx")
